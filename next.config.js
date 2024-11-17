9f58016258ad9c6f701b13f74df59bf75be2e52c
Clement Peleman <clementpeleman@outlook.com>
2024-11-17 20:54:21 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(ttf|woff|woff2|eot|otf)$/,
-        use: ["file-loader"],
-      });
-      return config;
-    },
+    optimizeFonts: false
 }
 
 module.exports = nextConfig

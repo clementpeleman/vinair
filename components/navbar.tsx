41bab7946067120b0c7a6dab8e9b5546d22dcc1e
Clement Peleman <clementpeleman@outlook.com>
2024-11-25 16:03:29 +0100

    modified:   components/navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -142,7 +142,7 @@ export default function App() {
           <>
             <Button
               as={Link}
-              className="underline"
+              className=""
               href="/login"
               radius="sm"
               variant="light"

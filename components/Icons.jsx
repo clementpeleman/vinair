fd030a55063ae945342b2430ce41d8fbcab31f8f
clementpeleman <clementpeleman@outlook.com>
2024-12-28 19:55:59 +0100

    logo

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -228,9 +228,9 @@ export const Logo = ({ size = 36, width, height, ...props }) => (
     width={size || width}
     {...props}
   >
-    <g id="Layer 1">
-      <path id="Vorm 1" fillRule="evenodd" fill="currentColor" d="m422.2 53h152l-76 134.3z"/>
-      <path id="Vorm 2" fillRule="evenodd" fill="currentColor" d="m21 53h127.2l208.6 420.6-58.3 118.4z"/>
+    <g fill="currentColor" >
+      <path fill="currentColor" d="M 422.215169 52.992188 L 574.222331 52.992188 L 498.21875 187.312663 Z M 422.215169 52.992188 " transform="matrix(0.0805369,0,0,0.0743034,0,0)"/>
+      <path fill="currentColor" d="M 21.001628 52.992188 L 148.223958 52.992188 L 356.785156 473.617676 L 298.485026 592.008952 Z M 21.001628 52.992188 " transform="matrix(0.0805369,0,0,0.0743034,0,0)"/>
     </g>
   </svg>
 );
